47d3e032fcb9be195152928e4e7bf764b9818fd6
A Fadila <57476855+fadilaaaa@users.noreply.github.com>
2024-06-30 20:24:36 +0700

    Update presensi.blade.php

--- resources/views/petugas/presensi.blade.php
+++ resources/views/petugas/presensi.blade.php
@@ -11,30 +11,44 @@
                         <h6 class="m-0 font-weight-bold text-primary">Data Presensi Petugas</h6>
                     </div>
                     <div class="card-body">
-                        <div id="cam"></div>
+                        <div id="cam" style="width: 280px;
+                        border:"></div>
+                        <button id="take" class="btn btn-success">Foto</button>
+                        <button class="btn btn-secondary" id="reset" hidden>Reset</button>
                     </div>
                 </div>
             </div>
         </div>
+        <input type="image" id="subIm">
     </div>
 @endsection
 
 @push('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
     <script>
-        Webcam.set({
-
-            width: 300,
-            height: 400,
-
-            image_format: 'jpeg',
-
-            jpeg_quality: 90
-
+        function initcam() {
+            Webcam.set({
+                width: 280,
+                height: 240,
+                image_format: 'jpeg',
+                jpeg_quality: 90
+            });
+            Webcam.attach('#cam');
+        }
+        document.getElementById('take').addEventListener('click', function() {
+            Webcam.snap(function(data_uri) {
+                document.getElementById('cam').innerHTML = '<img src="' + data_uri + '"/>';
+                document.getElementById('subIm').src = data_uri;
+            });
+            document.getElementById('reset').removeAttribute('hidden');
+            document.getElementById('take').setAttribute('hidden', true);
         });
-
-
-
-        Webcam.attach('#cam');
+        document.getElementById('reset').addEventListener('click', function() {
+            initcam();
+            document.getElementById('reset').setAttribute('hidden', true);
+            document.getElementById('take').removeAttribute('hidden');
+            document.getElementById('subIm').removeAttribute('src');
+        });
+        initcam();
     </script>
 @endpush
